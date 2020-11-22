0e2f8f59b38f84bf31ee66c7984291b703abeb02
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-11-22 17:58:20 +0000

    update

--- shell.h
+++ shell.h
@@ -1,8 +1,17 @@
 #ifndef shell_h
 #define shell_h
 
+/*Liberias*/
 #include <stdio.h>
 #include <unistd.h>
+#include <stlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/*prototipos*/
+int main(int ac, char **av);
+int main(int ac, char **av, char **env);
 
 
 #endif
