1830cfed34f0f5a8d9458d9f8a78a16729fac350
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-11-27 14:28:00 -0500

    Update Betty

--- main.c
+++ main.c
@@ -26,31 +26,31 @@ main(void){
     __fpurge(stdin); /*Limpiando el buffer de entrada de teclado*/
     memset(comando,'\0', MAX_I); /*Borrando cualquier contenido previo de comando*/
     scanf("%[^\n]s", comando); /*Esperar hasta que el usuario ingrese algun comando*/
-    if(strlen(comando)>0){ /*Actuar solo si comando contiene algo y solo un enter*/
+    if (strlen(comando)>0){ /*Actuar solo si comando contiene algo y solo un enter*/
       separaArgs(); /*Separar comando de sus argumentos*/
-      if(strcmp(comando,"cd") == 0){ /*Si el comando es cd*/
-        if(args[1]) /*Verificar que cuente con el argumento necesario*/
-    if(chdir(args[1]) != 0) /*La func chdir hace el cambio de directorio si regresa un valor 
+      if (strcmp(comando,"cd") == 0){ /*Si el comando es cd*/
+        if (args[1]) /*Verificar que cuente con el argumento necesario*/
+    if (chdir(args[1]) != 0) /*La func chdir hace el cambio de directorio si regresa un valor 
             diferente de cero la operacion no se pudo ejecutar con exito*/
-     printf("Error! %s no existe o no se puede cambiar a este directorio\n",args[1]);
-   else getcwd(PWD, MAX_I);/*En caso de cambio exitoso actualizar PWD*/
+    printf("Error! %s no existe o no se puede cambiar a este directorio\n",args[1]);
+    else getcwd(PWD, MAX_I);/*En caso de cambio exitoso actualizar PWD*/
       }
-      else if(strcmp(comando, "dir") == 0)
+      else if (strcmp(comando, "dir") == 0)
         listaDir(); /*Si el comando es el dir llamar a la func correspondiente*/
-      else if(strcmp(comando, "clr") == 0)
+      else if (strcmp(comando, "clr") == 0)
         strcpy(comando,"clear"), comExterno(); /*Limpia la pantalla con la
          funcion de comando externo y se pasa como comando clear.*/
-      else if(strcmp(comando, "env") == 0){
+      else if (strcmp(comando, "env") == 0){
         printf(" Variables de ambiente:\n");/*Mostrar los valores de las var de ambiente*/
         printf("  HOME=%s\n  PWD=%s\n", HOME, PWD);
         printf("  SHELL=%s\n  PATH=%s\n", SHELL, PATH);
       }
-      else if(strcmp(comando, "echo")==0){
-        if(args[1]) eco(); /*Si hay al menos 1 argumento llamar la func que ejecuta echo*/
+      else if (strcmp(comando, "echo") == 0){
+        if (args[1]) eco(); /*Si hay al menos 1 argumento llamar la func que ejecuta echo*/
       }
-      else if(strcmp(comando, "pwd")==0)
+      else if (strcmp(comando, "pwd") == 0)
         printf("%s\n", PWD); /*Mostrar el contenido de PWD (Directorio actual)*/
-      else if(strcmp(comando, "exit")==0)
+      else if (strcmp(comando, "exit") == 0)
         continuar = 0; /*Cambiar el valor de continuar para que termine*/
       else comExterno(); /*Cualquier otra entrada llamar a comExterno*/
     }
