87a11a691c834a8237049974552da3181f2e04e8
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-11-27 14:50:23 -0500

    Betty

--- holberton.h
+++ holberton.h
@@ -10,9 +10,9 @@
 #include <dirent.h>
 
 /*Declarando cabeceras de funciones*/
-void separaArgs(void); /*Esta funcion separa la cadena ingresada por el usuario en palabras individuales*/
+void separaArgs(void); /*Esta func separa una cadena en palabras individuales*/
 void listaDir(void); /*Esta función ejecuta el comando dir*/
 void eco(void); /*Esta función ejecuta el comando echo*/
-void comExterno(void); /*Esta función ejecuta lo que se considere comandos externos*/
+void comExterno(void); /*Func que ejecuta lo que se considere comandos externos*/
 
 #endif
