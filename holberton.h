c40bda4d93fe5b8ce5f4137e915f9b3eea60c540
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-11-27 13:35:28 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -9,4 +9,11 @@
 #include <unistd.h>
 #include <dirent.h>
 
+/*Declarando cabeceras de funciones*/
+void separaArgs(void); /*Esta funcion separa la cadena ingresada por el usuario en palabras individuales, 
+la 1° palabra sera considerada comando y el resto sus argumentos*/
+void listaDir(void); /*Esta función ejecuta el comando dir*/
+void eco(void); /*Esta función ejecuta el comando echo*/
+void comExterno(void); /*Esta función ejecuta lo que se considere comandos externos*/
+
 #endif
