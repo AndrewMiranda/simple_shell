74d114635556036cea827ef4f609d071c0e99908
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2020-11-27 14:35:31 -0500

    Batty 4.0

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 #define MAX_ARGS 13  /*Numero maximo de argumentos (-1) cuando se trate de un comando externo*/
 #define MAX_I 105 /*Numero de caracteres maximo para comando las variables de ambiente*/
 
-/*Declarando variables*/
-char comando[MAX_I]; /*Esta var lee el comando que ingrese el usuario*/
-char *args[MAX_ARGS]; /*en este arreglo se almacenan los argumentos del comando ingresado*/
-/*Declarando variables de ambiente*/
-char SHELL[MAX_I];
-char PATH[MAX_I];
-char HOME[MAX_I];
-char PWD[MAX_I];
-
 main(void) {
+  /*Declarando variables*/
+  char comando[MAX_I]; /*Esta var lee el comando que ingrese el usuario*/
+  char *args[MAX_ARGS]; /*en este arreglo se almacenan los argumentos del comando ingresado*/
+  /*Declarando variables de ambiente*/
+  char SHELL[MAX_I];
+  char PATH[MAX_I];
+  char HOME[MAX_I];
+  char PWD[MAX_I];
+  
   int continuar = 1;
   /*Inicializando variables de ambiente*/
   getcwd(PWD, MAX_I); /*Obteniendo la ruta actual y cargando en PWD*/
