96b57a0869093d1ad4c697a3e5306c8f39f93625
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-11-27 18:33:58 +0000

    libreriascommit

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef holberton_h
+#define holberton_h
+
+/* Librerias */
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#endif
